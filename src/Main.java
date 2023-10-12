1122a22afb4e2b2b55a9d5d2d68b24c7e88b01e1
rajeeva <rajeevabp92@gmail.com>
2023-10-12 14:21:07 +0530

    prototype

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+//        int [] arrayM = {1,2,3,0,0,0};
+//        int m = 3;
+//        int [] arrayN = {2,5,6};
+//        int n = 3;
+//        mergesort.merge(arrayM, m, arrayN, n);
+//
+//        Node root = new Node(1);
+//        root.left = new Node(2);
+//        root.right = new Node(3);
+//        root.left.left = new Node(4);
+//        root.right.right = new Node(6);
+//        root.left.right = new Node(5);
+//
+//        inorder.printInOrder(root);
+//
+//
+//        Node data = new Node(1);
+//        data.left = new Node(2);
+//        data.right = new Node(2);
+//        data.left.left = new Node(3);
+//        data.right.right = new Node(3);
+//        data.left.right = new Node(4);
+//        data.right.left = new Node(4);
+//        boolean result = symmetricTree.isSymmetric(data);
+//
+//        System.out.print("Symmetric Tree = " + result);
+//
+//        System.out.print("Max Depth = " + maxDepth.maxDepthNode(data));
+//        int [] arr = {1,2,4,5,7,8};
+//        Node Result = arrayTObst.sortedArrayToBST(arr);
+//        preOrder.preorder(Result);
+
+        String excelCol = excel.convertToTitle(45);
+        System.out.println("column number = "+ excelCol);
+
+    }
+}
