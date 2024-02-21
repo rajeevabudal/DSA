b3fdb8d439688cdbea0c05875f142adcc31977f0
rajeeva <rajeevabp92@gmail.com>
2024-02-21 22:20:35 +0530

    complered pascal with python

---
+++ pascal.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        row = [1] * (rowIndex + 1)
+
+        for i in range(2, rowIndex + 1):
+            for j in range(1, i):
+                row[i - j] +=row[i - j - 1]
+        return 
